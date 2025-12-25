ab2094f0802fc540877d839cb7da42c99503cf70
Bury Huang <buryhuang@gmail.com>
2025-12-25 00:28:32 -0800

    add js into build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   root: '.',
   publicDir: 'public',
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'js/**/*',
+          dest: 'js'
+        },
+        {
+          src: 'css/**/*',
+          dest: 'css'
+        }
+      ]
+    })
+  ],
   build: {
     outDir: 'dist',
     rollupOptions: {
